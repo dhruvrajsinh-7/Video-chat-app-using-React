6037e8da9f4776b8e0ed84f9ad851b8774238140
dhruvrajsinh <dhruvsss007@gmail.com>
2022-10-16 23:38:50 +0530

    file added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Typography,AppBar } from "@material-ui/core";
+import Videoplayer from "./components/Videoplayer";
+import Options from "./componets/Options";
+import Notification from "./componets/Notification";
+
 const App = () => {
   return (
     <div>
      <AppBar position="static" color="inherit">
         <Typography variant="h2" align="center">Video Chat</Typography>   
      </AppBar>
-
+    
     </div>
   )
   }
