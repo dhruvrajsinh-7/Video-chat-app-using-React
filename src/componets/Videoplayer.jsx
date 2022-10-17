d35eb45fb4549db1e7b03889f16cbc1ad84a163a
dhruvrajsinh <dhruvsss007@gmail.com>
2022-10-18 00:15:19 +0530

    changes due to error

--- src/componets/Videoplayer.jsx
+++ src/componets/Videoplayer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useContext} from "react";
 import { Grid,Typography,Paper } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles"; 
 import { SocketContext } from "./SocketContext";
@@ -23,25 +23,33 @@ const useStyles =makeStyles((theme)=>({
 }));
 const Videoplayer = () => {
   const classes = useStyles();
-
+  const context = useContext(SocketContext);
+  const {name,callAccepted,myVideo,userVideo,callEnded,stream,call} = context;
   return (
          <Grid container className={classes.gridContainer}>
               {/* Our Video */}
-                <Paper className={classes.paper}>
-                    <Grid item xs={12} md={6}>
-                        <Typography variant="h5" gutterBottom>Me</Typography>
-                        <video playsInline muted ref={'null'} autoPlay className={classes.video} />
-                    </Grid>
-                </Paper>   
-                {/* User Video */}
+              {
+                stream && (
                 <Paper className={classes.paper}>
                     <Grid item xs={12} md={6}>
                         <Typography variant="h5" gutterBottom>{name || 'Name'}</Typography>
+                        <video playsInline muted ref={'null'} autoPlay className={classes.video} />
+                    </Grid>
+                </Paper>   
+                )
+                }
+                {/* User Video */}
+                { callAccepted && !callEnded && (
+                <Paper className={classes.paper}>
+                    <Grid item xs={12} md={6}>
+                        <Typography variant="h5" gutterBottom>{call.name || 'Name'}</Typography>
                         <video playsInline ref={'userVideo'} autoPlay className={classes.video} />
                       </Grid>
                 </Paper>
+                )
+                }
             </Grid>
 
-  )
+      );
   }
   export default Videoplayer;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import './style.css';
-import {ContextProvider} from 'SocketContext';
-ReactDOM.render(
+import {ContextProvider} from './componets/SocketContext';
+import {createRoot} from 'react-dom/client';
+const rootElement = document.getElementById('root');
+root = createRoot(rootElement);
+root.render(
     <ContextProvider>
-        <App />
+        <App/>
     </ContextProvider>,
-    document.getElementById("root"));
+    document.getElementById('root')
+);
+
